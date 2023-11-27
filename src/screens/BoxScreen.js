f71afc21b8950f4d0208447b1716c610d03f1ffe
MCGaming1991 <mcgaming1991@gmail.com>
2023-11-26 22:58:15 -0600

    changes

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,10 +4,11 @@ import React from "react";
 const BoxScreen = (props) => {
   return (
     <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>Child 1</Text>
-      <Text style={styles.textStyle}>Child 2</Text>
-      <Text style={styles.textStyle}>Child 3</Text>
-      <Text style={styles.textStyle}>Child 4</Text>
+      <View style={styles.boxContainer}>
+        <View style={styles.box} />
+        <View style={styles.boxTwo} />
+      </View>
+      <View style={styles.boxThree} />
     </View>
   );
 };
@@ -16,11 +17,38 @@ export default BoxScreen;
 
 const styles = StyleSheet.create({
   viewStyle: {
-    borderWidth: 3,
+    width: "100%",
+    borderWidth: 1,
     borderColor: "black",
+    height: "50%",
+    flexDirection: "column",
+    display: "flex",
+    alignItems: "center",
   },
-  textStyle: {
+  boxContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+  },
+  box: {
     borderWidth: 3,
     borderColor: "red",
+    backgroundColor: "red",
+    height: 100,
+    width: 100,
+  },
+  boxTwo: {
+    borderWidth: 3,
+    borderColor: "purple",
+    backgroundColor: "purple",
+    height: 100,
+    width: 100,
+  },
+  boxThree: {
+    borderWidth: 3,
+    borderColor: "green",
+    backgroundColor: "green",
+    height: 100,
+    width: 100,
   },
 });
